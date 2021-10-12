86d1f3cf2c1d25825b8e0055183953dd3b25f9d7
sjjkk1996 <58585992+sjjkk1996@users.noreply.github.com>
2021-10-12 17:56:01 +0900

    10 07 oauth 적용한것

--- src/main/java/org/zerock/club/controller/SampleController.java
+++ src/main/java/org/zerock/club/controller/SampleController.java
@@ -1,6 +1,7 @@
 package org.zerock.club.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +14,31 @@ import javax.swing.plaf.PanelUI;
 @Log4j2
 @RequestMapping("/sample/")
 public class SampleController {
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll(){
         log.info("exAll.......");
     }
+    //@PreAuthorize("principal.username == #clubAuthMember.username")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/member")
     public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember){
         log.info("exMember.......");
         log.info("---------------------");
         log.info(clubAuthMember);
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public void exAdmin(){
         log.info("exAdmin.......");
+    }
+
+    @PreAuthorize("#clubAuthMember != null && #clubAuthMember.username eq" +
+            "\"user95@ds.com\"")
+    @GetMapping("/exOnly")
+    public String exMemberOnly(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO){
+        log.info("exMemberOnly....................");
+        log.info(clubAuthMemberDTO);
+        return "/sample/admin";
     }
 }
--- src/main/java/org/zerock/club/security/dto/ClubAuthMemberDTO.java
+++ src/main/java/org/zerock/club/security/dto/ClubAuthMemberDTO.java
@@ -6,24 +6,37 @@ import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 
 
+//보안 인증인가를 위해 사용되는 dto
 @Log4j2
 @Getter
 @Setter
 @ToString
-public class ClubAuthMemberDTO extends User {
+public class ClubAuthMemberDTO extends User implements OAuth2User {
     private String email;
+    private String password;
     private String name;
     private boolean fromSocial;
-
+    private Map<String, Object> attr;
     public ClubAuthMemberDTO(String username, String password, boolean fromSocial,
-                             Collection<? extends GrantedAuthority> authorities) {
+                             Collection<? extends GrantedAuthority> authorities, Map<String, Object> attr) {
+        this(username,password, fromSocial, authorities);
+        this.attr = attr;
+    }
+    public ClubAuthMemberDTO(String username, String password, boolean fromSocial, Collection<?
+            extends GrantedAuthority> authorities) {
         super(username, password, authorities);
         this.email = username;
+        this.password = password;
         this.fromSocial = fromSocial;
     }
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attr;
+    }
 }
-
